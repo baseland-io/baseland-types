5df46f0bb09d5c3403c1bbec8a2f371ee1b7ddbf
Zeph LaFassett! <zeph@lafassett.com>
2022-10-20 17:44:05 -0700

    [initial-setup] adding all of the stuff

---
+++ src/constants/rabbitmq/queues.ts
@@ -0,0 +1,13 @@
+export default {
+  CHARGE_BACK: 'transaction.chargeback',
+  LOGIN: 'event.login',
+  LOGOUT: 'event.logout',
+  PURCHASE: 'transaction.purchase',
+  REQUEST_INITIAL: 'request.initial',
+  REQUEST_RECUR: 'request.recur',
+  SIGNUP: 'event.signup',
+  SUBSCRIPTION_INITIAL: 'transaction.subscription.initial',
+  SUBSCRIPTION_CANCEL: 'transaction.subscription.cancel',
+  SUBSCRIPTION_RECUR: 'transaction.subscription.recur',
+  REFUND: 'transaction.refund',
+};
